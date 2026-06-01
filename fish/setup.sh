20cc6cceb7f26995a3e36d3466f2baac0e3ec3ff
Park Changyong <me@changyong.me>
2026-06-02 02:30:57 +0900

    Set Homebrew fish as default shell

--- fish/setup.sh
+++ fish/setup.sh
@@ -1,3 +1,9 @@
 mkdir -p ~/.config/fish
 ln -snf ~/.dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -snf ~/.dotfiles/fish/starship.toml ~/.config/starship.toml
+
+if ! grep -qxF /opt/homebrew/bin/fish /etc/shells; then
+  echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells >/dev/null
+fi
+
+chsh -s /opt/homebrew/bin/fish

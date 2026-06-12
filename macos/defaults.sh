a9d8fa1a7242c3e8dd992f1273bf2eaf197e1770
Park Changyong <me@changyong.me>
2026-06-12 22:01:44 +0900

    Configure system defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# Dock
+# Dock preferences
 defaults write com.apple.dock orientation -string "left"
 defaults write com.apple.dock tilesize -int 48
 defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock mineffect -string "scale"
 defaults write com.apple.dock size-immutable -bool true
 
-# Finder
+# Finder preferences
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
@@ -16,17 +16,17 @@ defaults write com.apple.finder _FXSortFoldersFirst -bool true
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Desktop
+# Desktop preferences
 defaults write com.apple.finder _FXSortFoldersFirstOnDesktop -bool true
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
-# Keyboard
+# Keyboard preferences
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write com.apple.HIToolbox AppleFnUsageType -int 0
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 
-# Mission Control
+# Mission control preferences
 defaults write com.apple.dock mru-spaces -bool false
 
 # Miscellaneous

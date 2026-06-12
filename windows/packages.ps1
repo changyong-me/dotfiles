5d18be5d337fed973629d4eabc31e3ddcb4ceea9
Park Changyong <me@changyong.me>
2026-06-12 18:30:26 +0900

    Improve Windows package bootstrap idempotency

--- windows/packages.ps1
+++ windows/packages.ps1
@@ -1,26 +1,40 @@
 $ErrorActionPreference = "Stop"
 
-function Install-App($Id, $Location) {
+function Add-ScoopBucket($Name) {
+  if (-not (scoop bucket list | Where-Object Name -eq $Name)) {
+    scoop bucket add $Name
+  }
+}
+
+function Install-ScoopPackage($Name) {
+  if (scoop list | Where-Object Name -eq ($Name -split "/")[-1]) {
+    scoop update $Name
+  } else {
+    scoop install $Name
+  }
+}
+
+function Install-WinGetPackage($Id, $Location) {
   $extra = if ($Location) { @("--location", $Location) } else { @() }
   winget install --id $Id -e --accept-source-agreements --accept-package-agreements @extra
 }
+
+Add-ScoopBucket nerd-fonts
+Install-ScoopPackage nerd-fonts/D2Coding-NF
+Install-ScoopPackage nerd-fonts/Hack-NF
+Install-ScoopPackage pwsh
+Install-ScoopPackage starship
 
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Repair-WinGetPackageManager -Force -Latest
 
-Install-App AdGuard.AdGuard
-Install-App Blizzard.BattleNet "${env:ProgramFiles(x86)}\Battle.net"
-Install-App Discord.Discord
-Install-App Google.Chrome
-Install-App Modrinth.ModrinthApp
-Install-App RazerInc.RazerInstaller.Synapse4
-Install-App Valve.Steam
-Install-App ZedIndustries.Zed
-Install-App 9PLM9XGG6VKS # Codex
-
-scoop bucket add nerd-fonts
-scoop install nerd-fonts/D2Coding-NF
-scoop install nerd-fonts/Hack-NF
-scoop install pwsh
-scoop install starship
+Install-WinGetPackage AdGuard.AdGuard
+Install-WinGetPackage Blizzard.BattleNet "${env:ProgramFiles(x86)}\Battle.net"
+Install-WinGetPackage Discord.Discord
+Install-WinGetPackage Google.Chrome
+Install-WinGetPackage Modrinth.ModrinthApp
+Install-WinGetPackage RazerInc.RazerInstaller.Synapse4
+Install-WinGetPackage Valve.Steam
+Install-WinGetPackage ZedIndustries.Zed
+Install-WinGetPackage 9PLM9XGG6VKS # Codex
